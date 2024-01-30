51f9a90f7ee0b6bad2064489f835bc4c3196082d
uzz <uzziube@gmail.com>
2024-01-30 12:55:14 +0530

    'first'

---
+++ apiLayer/Controllers/DemoapiController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiLayer.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class DemoapiController : ControllerBase
+    {
+        // GET: api/<DemoapiController>
+        [HttpGet]
+        [Route("GetDet")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<DemoapiController>/5
+        [HttpGet("GetbyId/{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DemoapiController>
+        [HttpPost("send")]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DemoapiController>/5
+        [HttpPut("update/{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DemoapiController>/5
+        [HttpDelete("delete/{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
